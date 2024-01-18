0be23cad0605f0b65e62d2f1f3a97518477989f3
gabe <gabrieltucker260@gmail.com>
2024-01-18 11:21:21 -0800

    added 2/3 players

--- RPS/Models/RPS.cs
+++ RPS/Models/RPS.cs
@@ -1,71 +1,138 @@
 namespace RockPaperScissors.Models
 {
-    public class RPS
+  public class RPS
+  {
+    public string Player1 { get; set; }
+    public string Player2 { get; set; }
+    public string Player3 { get; set; }
+
+    public RPS(string firstRoll, string secondRoll, string thirdRoll)
     {
-        public string Player1 { get; set; }
-        public string Player2 { get; set; }
-        public string Player3 { get; set; }
-
-        public RPS(string firstRoll, string secondRoll, string thirdRoll)
-        {
-            Player1 = firstRoll;
-            Player2 = secondRoll;
-            Player3 = thirdRoll;  
-        }
-
-        public string Play()
-        {
-            if (Player1 == Player2 && Player2 == Player3)
-            {
-                return "It's a draw";
-            }
-            else if (Player1 == Player2)
-            {
-                return "Player1 and Player2 have drawn";
-            }
-            else if (Player1 == Player3)
-            {
-                return "Player1 and Player3 have drawn";
-            }
-            else if (Player2 == Player3)
-            {
-                return "Player2 and Player3 have drawn";
-            }
-            else if (Player1 == "rock" && Player2 == "scissors" && Player3 == "scissors")
-            {
-                return "player 1 wins";
-            }
-            else if (Player1 == "paper" && Player2 == "rock" && Player3 == "rock")
-            {
-                return "player 1 wins";
-            }
-            else if (Player1 == "scissors" && Player2 == "paper" && Player3 == "paper")
-            {
-                return "player 1 wins";
-            }
-            else if (Player2 == "rock" && Player1 == "scissors" && Player3 == "scissors")
-            {
-                return "player 2 wins";
-            }
-            else if (Player2 == "paper" && Player1 == "rock" && Player3 == "rock")
-            {
-                return "player 2 wins";
-            }
-            else if (Player2 == "scissors" && Player1 == "paper" && Player3 == "paper")
-            {
-                return "player 2 wins";
-            }
-            else if (Player1 == "rock" && Player2 == "paper" && Player3 == "scissors" || Player2 == "rock" && Player3 == "paper" && Player1 == "scissors"|| Player3 == "rock" && Player1 == "paper" && Player2 == "scissors" )
-            {
-              return "No one wins";
-            }
-            else 
-            {
-                return "Player 3 Wins";
-            }
-        }
+      Player1 = firstRoll;
+      Player2 = secondRoll;
+      Player3 = thirdRoll;
     }
+
+    public RPS(string firstRoll, string secondRoll)
+    {
+      Player1 = firstRoll;
+      Player2 = secondRoll;
+    }
+
+    public string TwoPlay()
+    {
+      if (Player1 == Player2)
+      {
+        return "It's a draw";
+      }
+      else if (
+                 (Player1 == "rock") &&
+                 (Player2 == "scissors") ||
+                 (Player1 == "paper") &&
+                 (Player2 == "rock") ||
+                 (Player1 == "scissors") &&
+                 (Player2 == "paper")
+               )
+      {
+        return "player 1 wins";
+      }
+      else
+      {
+        return "player 2 wins";
+      }
+    }
+
+    public string Play()
+    {
+      if (Player1 == Player2 && Player2 == Player3)
+      {
+        return "It's a draw";
+      }
+      else if (Player1 == Player2)
+      {
+        return "Player1 and Player2 have drawn";
+      }
+      else if (Player1 == Player3)
+      {
+        return "Player1 and Player3 have drawn";
+      }
+      else if (Player2 == Player3)
+      {
+        return "Player2 and Player3 have drawn";
+      }
+      else if (Player1 == "rock" && Player2 == "scissors" && Player3 == "scissors")
+      {
+        return "player 1 wins";
+      }
+      else if (Player1 == "paper" && Player2 == "rock" && Player3 == "rock")
+      {
+        return "player 1 wins";
+      }
+      else if (Player1 == "scissors" && Player2 == "paper" && Player3 == "paper")
+      {
+        return "player 1 wins";
+      }
+      else if (Player2 == "rock" && Player1 == "scissors" && Player3 == "scissors")
+      {
+        return "player 2 wins";
+      }
+      else if (Player2 == "paper" && Player1 == "rock" && Player3 == "rock")
+      {
+        return "player 2 wins";
+      }
+      else if (Player2 == "scissors" && Player1 == "paper" && Player3 == "paper")
+      {
+        return "player 2 wins";
+      }
+      else if (Player1 == "rock" && Player2 == "paper" && Player3 == "scissors" || Player2 == "rock" && Player3 == "paper" && Player1 == "scissors" || Player3 == "rock" && Player1 == "paper" && Player2 == "scissors")
+      {
+        return "No one wins";
+      }
+      else
+      {
+        return "Player 3 Wins";
+      }
+    }
+  }
 }
+
+
+// {
+//   public class RPS
+//   {
+//     public string Player1 { get; set; }
+//     public string Player2 { get; set; }
+
+//     public RPS(string firstRoll, string secondRoll)
+//     {
+//       Player1 = firstRoll;
+//       Player2 = secondRoll;
+//     }
+
+//     public string Play()
+//     {
+//       if (Player1 == Player2)
+//       {
+//         return "It's a draw";
+//       }
+//       else if (
+//         (Player1 == "Rock" || Player1 == "rock" || Player1 == "ROCK") &&
+//         (Player2 == "Scissors" || Player2 == "scissors" || Player2 == "SCISSORS") ||
+//         (Player1 == "Paper" || Player1 == "PAPER" || Player1 == "paper") &&
+//         (Player2 == "Rock" || Player2 == "ROCK" || Player2 == "rock") ||
+//         (Player1 == "Scissors" || Player1 == "scissors" || Player1 == "SCISSORS") &&
+//         (Player2 == "Paper" || Player2 == "PAPER" || Player2 == "paper")
+//       )
+//       {
+//         return "player 1 wins";
+//       }
+//       else
+//       {
+//         return "player 2 wins";
+//       }
+//     }
+//   }
+// }
 
 // Player Wins
 // Rock, scissors, scissors
--- RPS/Program.cs
+++ RPS/Program.cs
@@ -8,16 +8,41 @@ namespace ExampleName
     static void Main()
     {
       Console.WriteLine("This is a Rock Paper Scissors game.");
-      Console.WriteLine("Please enter Rock, Paper, or Scissors.");
-      Console.WriteLine("Player 1's Roll");
-      string player1 = Console.ReadLine().ToLower();
-      Console.WriteLine("Player 2's Roll");
-      string player2 = Console.ReadLine().ToLower();
-      Console.WriteLine("Player 3's Roll");
-      string player3 = Console.ReadLine().ToLower();
-      RPS game = new RPS(player1, player2, player3);
-      string result = game.Play();
-      Console.WriteLine(result);
+      PlayersInput();
+      static void PlayersInput()
+      {
+        Console.WriteLine("Are you playing with '2' or '3' players? Enter below:");
+        string gameType = Console.ReadLine();
+        if(gameType == "2")
+        {
+        Console.WriteLine("Please enter Rock, Paper, or Scissors.");
+        Console.WriteLine("Player 1's Roll");
+        string player1 = Console.ReadLine().ToLower();
+        Console.WriteLine("Player 2's Roll");
+        string player2 = Console.ReadLine().ToLower();
+        RPS game = new RPS(player1, player2);
+        string result = game.TwoPlay();
+        Console.WriteLine(result);
+        } 
+        else if (gameType == "3")
+        {
+        Console.WriteLine("Please enter Rock, Paper, or Scissors.");
+        Console.WriteLine("Player 1's Roll");
+        string player1 = Console.ReadLine().ToLower();
+        Console.WriteLine("Player 2's Roll");
+        string player2 = Console.ReadLine().ToLower();
+        Console.WriteLine("Player 3's Roll");
+        string player3 = Console.ReadLine().ToLower();
+        RPS game = new RPS(player1, player2, player3);
+        string result = game.Play();
+        Console.WriteLine(result);
+        }
+        else
+        {
+        Console.WriteLine("Please enter a valid number of players");
+        PlayersInput();
+        }
+      }
     }
   }
 }
